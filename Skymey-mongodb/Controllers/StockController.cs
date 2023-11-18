e905730472b9d04d243818015fafb526af533d6c
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-18 12:09:46 +0300

    add endpoint for stock dividends

--- Skymey-mongodb/Controllers/StockController.cs
+++ Skymey-mongodb/Controllers/StockController.cs
@@ -3,6 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using MongoDB.Driver;
 using MongoDB.Driver.Core.Configuration;
 using Skymey_main_lib.Models;
+using Skymey_main_lib.Models.CryptoCurrentPricesView;
+using Skymey_main_lib.Models.Dividends;
+using Skymey_main_lib.Models.Dividends.Polygon;
+using Skymey_main_lib.Models.Prices;
 using Skymey_main_lib.Models.Prices.StockPrices;
 using Skymey_main_lib.Models.Tickers;
 using Skymey_main_lib.Models.Tickers.Polygon;
@@ -30,6 +34,30 @@ namespace Skymey_mongodb.Controllers
         public async Task<List<TickerList>> GetShares()
         {
             return (from i in _db.TickerList select i).ToList();
+        }
+        [HttpGet]
+        [Route("GetDividends")]
+        public async Task<List<StockDividends>> GetDividends(string ticker)
+        {
+            List<StockDividends> cp = new List<StockDividends>();
+            var in_db = (from i in _db.DividendsPolygon where i.ticker == ticker select i);
+            foreach (var item in in_db)
+            {
+                StockDividends? div = new StockDividends();
+                div.Ticker = item.ticker;
+                div.Cash_amount = item.cash_amount;
+                div.Update = item.Update;
+                div.Currency = item.currency;
+                div.Declaration_date = item.declaration_date;
+                div.Dividend_type = item.dividend_type;
+                div.Ex_dividend_date = item.ex_dividend_date;
+                div.Frequency = item.frequency;
+                div.Pay_date = item.pay_date;
+                div.Record_date = item.record_date;
+                cp.Add(div);
+                div = null;
+            }
+            return cp;
         }
         ~StockController() { }
     }
--- Skymey-mongodb/Data/ApplicationContext.cs
+++ Skymey-mongodb/Data/ApplicationContext.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using MongoDB.EntityFrameworkCore.Extensions;
 using Skymey_main_lib.Models;
+using Skymey_main_lib.Models.Dividends.Polygon;
 using Skymey_main_lib.Models.Prices;
 using Skymey_main_lib.Models.Prices.Okex;
 using Skymey_main_lib.Models.Prices.StockPrices;
@@ -15,6 +16,7 @@ namespace Skymey_mongodb.Data
         public DbSet<StockPrices> StockPrices { get; init; }
         public DbSet<CurrentPrices> CryptoCurrentPrices { get; init; }
         public DbSet<OkexCurrentPrices> OkexCurrentPricesView { get; init; }
+        public DbSet<DividendsPolygon> DividendsPolygon { get; init; }
         public DbSet<TickerList> TickerList { get; init; }
         public static ApplicationContext Create(IMongoDatabase database) =>
             new(new DbContextOptionsBuilder<ApplicationContext>()
@@ -32,6 +34,7 @@ namespace Skymey_mongodb.Data
             modelBuilder.Entity<CurrentPrices>().ToCollection("crypto_current_prices");
             modelBuilder.Entity<OkexCurrentPrices>().ToCollection("crypto_current_okex_prices");
             modelBuilder.Entity<TickerList>().ToCollection("stock_tickerlist");
+            modelBuilder.Entity<DividendsPolygon>().ToCollection("stock_dividends");
         }
     }
 }
